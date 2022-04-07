fed9c806d2fec0dc9f5d2bbe3ef216a3441d878a
grsLammy <grspro98@gmail.com>
2022-04-07 15:20:37 +0530

    :sparkles: temporary replaced network config
    temporary replaced network config from polygon to ethereum testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,28 +2,19 @@ require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-const { POLYGON_MUMBAI_RPC_PROVIDER, SIGNER_PRIVATE_KEY, POLYGONSCAN_API_KEY } = process.env; 
+const { RPC_PROVIDER, SIGNER_PRIVATE_KEY, EXPLORER_API_KEY } = process.env; 
 
 module.exports = {
-  solidity: "0.8.4",
-  defaultNetwork: "mumbai",
-  networks: {
-    hardhat: {
-    },
-    polygon_mumbai: {
-      url: POLYGON_MUMBAI_RPC_PROVIDER,
-      accounts: [`0x${SIGNER_PRIVATE_KEY}`]
-  
-    },
-  },
+	solidity: "0.8.4",
+	networks: {
+		hardhat: {
+		},
+		rinkeby: {
+      		url: RPC_PROVIDER,
+      		accounts: [SIGNER_PRIVATE_KEY]
+    	},
+  	},
+  	etherscan: {
+    	apiKey: EXPLORER_API_KEY
+  	}
 };
