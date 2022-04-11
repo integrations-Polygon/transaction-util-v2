55c2bdb35403163b16c56c4efd948cbd034911f1
grsLammy <grspro98@gmail.com>
2022-04-11 13:02:25 +0530

    :zap: improved the code flow
    improved the code flow

--- scripts/utils/saveReceipt.js
+++ scripts/utils/saveReceipt.js
@@ -6,16 +6,16 @@ const saveReceipt = async (mappedReceipt) => {
             let existingLog = fs.readFileSync("log/log.json", "utf-8")
             let parseLog = JSON.parse(existingLog)
             parseLog.push(mappedReceipt)
-            appendExistingLog = await JSON.stringify(parseLog, null, 2) // indentation by 2
+            appendExistingLog = await JSON.stringify(parseLog, null, 2)
             fs.writeFileSync("log/log.json", appendExistingLog, "utf-8")
-        } else {
-            fs.writeFileSync("log/log.json", "[]", "utf-8")
-            let emptyArrayLog = fs.readFileSync("log/log.json", "utf-8")
-            let parseLog = JSON.parse(emptyArrayLog)
-            parseLog.push(mappedReceipt)
-            newLog = await JSON.stringify(parseLog, null, 2) // indentation by 2
-            fs.writeFileSync("log/log.json", newLog, "utf-8")
+            return
         }
+        fs.writeFileSync("log/log.json", "[]", "utf-8")
+        let emptyArrayLog = fs.readFileSync("log/log.json", "utf-8")
+        let parseLog = JSON.parse(emptyArrayLog)
+        parseLog.push(mappedReceipt)
+        newLog = await JSON.stringify(parseLog, null, 2)
+        fs.writeFileSync("log/log.json", newLog, "utf-8")
         console.log("\nTransaction receipt has been logged successfully.\n")
     } catch (e) {
         console.error(e)
