7b36c03747510f9749a31443794b196ba7711a7e
grsLammy <grspro98@gmail.com>
2022-04-13 13:44:28 +0530

    :sparkles: now works on polygon mumbai testnet
    now works on polygon mumbai testnet

--- scripts/send.js
+++ scripts/send.js
@@ -6,8 +6,12 @@ const redisDB = require("./utils/redisDB")
 const isNumeric = require("./utils/isNumeric")
 const saveReceipt = require("./utils/saveReceipt")
 const dataMapping = require("./utils/dataMapping")
-const { fetchGasPrice } = require("./utils/fetchData")
 const waitForConfirmation = require("./utils/waitForComfirmation")
+
+const {
+    fetchGasPriceLegacy,
+    fetchGasPriceEIP1559,
+} = require("./utils/fetchData")
 
 const network = process.env.NETWORK
 const projectID = process.env.PROJECT_ID
@@ -17,7 +21,7 @@ const walletAddress = process.env.PUBLIC_KEY
 const accountTransfer = async (
     txType,
     receiverAddress,
-    amountInETH,
+    amountInMATIC,
     network,
     projectID
 ) => {
@@ -29,15 +33,11 @@ const accountTransfer = async (
         const provider = new ethers.providers.InfuraProvider(network, projectID)
         const signer = new ethers.Wallet(pKey, provider)
 
-        // Retry sending transaction utill success, 10 retries max
-        while (txReceipt === null && retry < 10) {
-            const gasData = await fetchGasPrice()
-            gasInGWEI = gasData.fastest
-            gasPrice = gasInGWEI * 10 ** 9
-
-            // Convert the amount in ETH to WEI
+        // Retry sending transaction utill success, 5 retries max
+        while (txReceipt === null && retry < 5) {
+            // Convert the amount in MATIC to WEI
             const amount = ethers.BigNumber.from(
-                ethers.utils.parseEther(amountInETH)
+                ethers.utils.parseEther(amountInMATIC)
             )
 
             const nonce = await provider.getTransactionCount(walletAddress)
@@ -47,35 +47,25 @@ const accountTransfer = async (
                 txType,
                 receiverAddress,
                 amount,
-                nonce,
-                gasPrice
+                nonce
             )
 
-            console.log(
-                `Your transaction is being mined and the gas price being used is ${gasInGWEI} GWEI.`
-            )
             console.log(`The generated transaction hash is ${txHash}\n`)
             console.log("You can check your transaction at:")
-            console.log(`https://rinkeby.etherscan.io/tx/${txHash}\n`)
-            console.log(
-                "Waiting for 12 Block Confirmations (checks every 15 second)\n"
-            )
+            console.log(`https://mumbai.polygonscan.com/tx/${txHash}\n`)
+            console.log("Waiting for 64 Block Confirmations\n")
 
             // Wait for confirmation and get the txReceipt or null
-            txReceipt = await waitForConfirmation(provider, txHash)
+            txReceipt = await waitForConfirmation(provider, txHash, txType)
             if (txReceipt === null) {
                 retry += 1
                 console.log("\nTransaction failed...Trying again!\n")
             }
         }
         // Return the succeed txReceipt
-        if (txReceipt !== null) {
-            console.log(
-                "Transaction was mined successfully and confirmed by 12 blocks"
-            )
-            return txReceipt
-        }
-        console.log("Transaction failed even after 10 retries")
+        if (txReceipt !== null) return txReceipt
+
+        console.log("Transaction failed even after 5 retries")
         // Return the failed txReceipt
         return (txReceipt = await provider.getTransactionReceipt(txHash))
     } catch (error) {
@@ -89,33 +79,46 @@ const handleTransaction = async (
     txType,
     receiverAddress,
     amount,
-    nonce,
-    gasPrice
+    nonce
 ) => {
     if (txType === "1") {
+        const gasData = await fetchGasPriceLegacy()
+        let maxFeeInGWEI = gasData.fastest
+        const maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
         const txPayload = {
             type: 1,
             to: receiverAddress,
             value: amount,
             nonce: nonce,
             gasLimit: 21000,
-            gasPrice: gasPrice,
+            gasPrice: maxFee,
         }
         const tx = await signer.sendTransaction(txPayload)
+        console.log(
+            `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI.`
+        )
         return tx.hash
     }
 
     if (txType === "2") {
+        const gasData = await fetchGasPriceEIP1559()
+        let maxFeeInGWEI = gasData.fast.maxFee
+        let maxPriorityFeeInGWEI = gasData.fast.maxPriorityFee
+        const maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
+        const maxPriorityFee = Math.trunc(maxPriorityFeeInGWEI * 10 ** 9)
         const txPayload = {
             type: 2,
             to: receiverAddress,
             value: amount,
             nonce: nonce,
             gasLimit: 21000,
-            maxPriorityFeePerGas: gasPrice,
-            maxFeePerGas: gasPrice,
+            maxPriorityFeePerGas: maxPriorityFee,
+            maxFeePerGas: maxFee,
         }
         const tx = await signer.sendTransaction(txPayload)
+        console.log(
+            `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI.`
+        )
         return tx.hash
     }
     console.log(`Unsupported transaction type ${txType}.`)
@@ -136,13 +139,13 @@ async function startTransaction() {
     if (receiverAddress.length !== 42)
         return console.log(`${receiverAddress} is not a valid address`)
 
-    const amountInETH = prompt("Enter the amount of ETH to transfer: ")
-    if (!amountInETH) return console.log("Transfer amount cannot be null")
-    if (isNumeric(amountInETH) === false)
+    const amountInMATIC = prompt("Enter the amount of MATIC to transfer: ")
+    if (!amountInMATIC) return console.log("Transfer amount cannot be null")
+    if (isNumeric(amountInMATIC) === false)
         return console.log("Invalid transfer amount")
 
     const confirmation = prompt(
-        `Are you sure you want to transfer ${amountInETH} ETH to ${receiverAddress}? (Y/N): `
+        `Are you sure you want to transfer ${amountInMATIC} MATIC to ${receiverAddress}? (Y/N): `
     )
     if (confirmation !== "Y" && confirmation !== "y") return
 
@@ -151,7 +154,7 @@ async function startTransaction() {
     const txReceipt = await accountTransfer(
         txType,
         receiverAddress,
-        amountInETH,
+        amountInMATIC,
         network,
         projectID
     )
