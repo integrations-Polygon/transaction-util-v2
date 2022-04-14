da54271a00c8511cb7d772e3d0e967b314fd60e0
SaifAtom <95500725+SaifAtom@users.noreply.github.com>
2022-04-14 07:46:22 +0100

    fix: Improved UX

--- scripts/startTransaction.js
+++ scripts/startTransaction.js
@@ -1,39 +1,36 @@
+const ps = require("prompt-sync")
+const prompt = ps()
 const call = require("./main/call/call")
 const deploy = require("./main/deploy/deploy")
 const send = require("./main/send/send")
-const saveReceipt = require("./utils/saveReceipt")
-const dataMapping = require("./utils/dataMapping")
-const redisDB = require("./utils/redisDB")
+const saveReceipt = require("./main/utils/saveReceipt")
+const dataMapping = require("./main/utils/dataMapping")
+const redisDB = require("./main/utils/redisDB")
 
 
 
 async function startTransaction(){
+
     console.log("1. Send MATIC to a receiving address.")
     console.log("2. Deploy & verify your smart contract.")
     console.log("3. Call a function/method from your deployed smart contract.")
+
     const choice = prompt(
         "Select a transaction to proceed:-\n"
     )
+
+    if (!choice) return console.log("Choice cannot be null")
+
     if(choice !== "1" && choice !== "2" && choice !== "3") 
         return console.log(`Transaction ${choice} is unsupported`)
     
-    if(choice === "1") 
-        txReceipt = send()
-        mappedReceipt = await dataMapping(txReceipt)
-        await saveReceipt(mappedReceipt)
-        await redisDB(mappedReceipt)
-
-    if(choice === "2") 
-        txReceipt = deploy()
-        const mappedReceipt = await dataMapping(txReceipt)
-        await saveReceipt(mappedReceipt)
-        await redisDB(mappedReceipt)
+    if (choice === "1") txReceipt = send()
+    if (choice === "2") txReceipt = deploy()
+    if (choice === "3") txReceipt = call()
         
-    if(choice === "3")
-        txReceipt = call()
-        mappedReceipt = await dataMapping(txReceipt)
-        await saveReceipt(mappedReceipt)
-        await redisDB(mappedReceipt)
+    const mappedReceipt = await dataMapping(txReceipt)
+    await saveReceipt(mappedReceipt)
+    await redisDB(mappedReceipt)
 }
 
 startTransaction()
