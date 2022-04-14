0c7c0e20dae3ee43197755265f3d2d30c74ed0b7
grsLammy <grspro98@gmail.com>
2022-04-14 21:41:46 +0530

    :zap: increased wait time to 15sec
    increased wait time to 15sec

--- scripts/main/utils/waitForComfirmation.js
+++ scripts/main/utils/waitForComfirmation.js
@@ -2,16 +2,16 @@ const sleep = require("./sleep")
 const isConfirmed = require("./isConfirmed")
 
 /*  
-    - Total wait time is 15mins (10000ms  * 90)
-    - Every 10 seconds check for the 64 block confirmations status
-    - If the tx hash gets confirmed by 64 blocks within 15 mins then return the tx receipt
+    - Total wait time is 16mins (15000ms  * 64)
+    - Every 15 seconds check for the block confirmations status
+    - If the tx hash gets confirmed by 64 blocks within 16 mins then return the tx receipt
     - Else return null 
 */
 
 const waitForConfirmation = async (provider, txHash) => {
     try {
         let i = 0
-        while (i < 90) {
+        while (i < 64) {
             if (await isConfirmed(provider, txHash, 64)) {
                 console.log("\n")
                 console.log(
@@ -22,7 +22,7 @@ const waitForConfirmation = async (provider, txHash) => {
                 if (txReceipt !== null) return txReceipt
             }
             i += 1
-            await sleep(10000)
+            await sleep(15000)
         }
         return null
     } catch (error) {
