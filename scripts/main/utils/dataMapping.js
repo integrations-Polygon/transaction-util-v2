1df7847905aec49477a30d04dabad044ecf7d4d4
grsLammy <grspro98@gmail.com>
2022-04-14 21:39:33 +0530

    :zap: handled the error when mapping for deployment
    handled the error when mapping for deployment

--- scripts/main/utils/dataMapping.js
+++ scripts/main/utils/dataMapping.js
@@ -1,4 +1,20 @@
 const dataMapping = async (txReceipt) => {
+    if (txReceipt.to === null) {
+        const mappedReceipt = {
+            status: txReceipt.status,
+            type: txReceipt.type,
+            from: txReceipt.from,
+            to: txReceipt.to,
+            contractAddress: txReceipt.contractAddress,
+            blockHash: txReceipt.blockHash,
+            blockNumber: txReceipt.blockNumber,
+            transactionHash: txReceipt.transactionHash,
+            cumulativeGasUsed: txReceipt.cumulativeGasUsed.toString(),
+            effectiveGasPrice: txReceipt.effectiveGasPrice.toString(),
+            gasUsed: txReceipt.gasUsed.toString(),
+        }
+        return mappedReceipt
+    }
     const mappedReceipt = {
         status: txReceipt.status,
         type: txReceipt.type,
