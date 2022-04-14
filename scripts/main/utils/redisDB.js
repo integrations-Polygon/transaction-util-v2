cbdfa5a413f5242e889ed7270d453b8410a89b14
grsLammy <grspro98@gmail.com>
2022-04-14 21:40:45 +0530

    :zap: handled error when storing for deployment
    handled error when storing for deployment

--- scripts/main/utils/redisDB.js
+++ scripts/main/utils/redisDB.js
@@ -4,6 +4,20 @@ const redisClient = Redis.createClient()
 const redisDB = async (mappedReceipt) => {
     try {
         await redisClient.connect()
+        if (mappedReceipt.to === null) {
+            await redisClient.HSET(mappedReceipt.transactionHash, {
+                status: mappedReceipt.status,
+                type: mappedReceipt.type,
+                from: mappedReceipt.from,
+                contractAddress: mappedReceipt.contractAddress,
+                blockHash: mappedReceipt.blockHash,
+                blockNumber: mappedReceipt.blockNumber,
+                cumulativeGasUsed: mappedReceipt.cumulativeGasUsed,
+                effectiveGasUsed: mappedReceipt.effectiveGasPrice,
+                gasUsed: mappedReceipt.gasUsed,
+            })
+            return
+        }
         await redisClient.HSET(mappedReceipt.transactionHash, {
             status: mappedReceipt.status,
             type: mappedReceipt.type,
