2f7328f1a6e2288f37293706454f2874608b96a0
grsLammy <grspro98@gmail.com>
2022-04-20 09:36:42 +0530

    feat: dummy smart contract with constructor args

---
+++ contracts/Demo_args.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+import "./Demo.sol";
+
+contract Demo_args {
+    string private name;
+    uint256 private number;
+    Demo private demoAddress;
+
+    constructor(
+        string memory _name,
+        uint256 _number,
+        Demo _demoAddress
+    ) {
+        name = _name;
+        number = _number;
+        demoAddress = _demoAddress;
+    }
+
+    function getName() external view returns (string memory) {
+        return name;
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function getDemoAddress() external view returns (Demo) {
+        return demoAddress;
+    }
+}
