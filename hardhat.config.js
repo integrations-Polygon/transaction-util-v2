17e8dc876a70c111af825f6c2c609728c9f90beb
grsLammy <grspro98@gmail.com>
2022-04-21 09:55:47 +0530

    feat: added matic defaultNetwork

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,19 +4,20 @@ require("@nomiclabs/hardhat-etherscan")
 const { RPC_PROVIDER, SIGNER_PRIVATE_KEY, EXPLORER_API_KEY } = process.env
 
 module.exports = {
-    solidity: "0.8.4",
-    networks: {
-        hardhat: {},
-        matic: {
-            url: RPC_PROVIDER,
-            accounts: [SIGNER_PRIVATE_KEY],
-        },
-        mumbai: {
-            url: RPC_PROVIDER,
-            accounts: [SIGNER_PRIVATE_KEY],
-        },
+  solidity: "0.8.4",
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {},
+    matic: {
+      url: RPC_PROVIDER,
+      accounts: [SIGNER_PRIVATE_KEY],
     },
-    etherscan: {
-        apiKey: EXPLORER_API_KEY,
+    mumbai: {
+      url: RPC_PROVIDER,
+      accounts: [SIGNER_PRIVATE_KEY],
     },
+  },
+  etherscan: {
+    apiKey: EXPLORER_API_KEY,
+  },
 }
