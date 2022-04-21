a09a7d875098d97fd9dbb3fbb8f2ba58cccae8ce
grsLammy <grspro98@gmail.com>
2022-04-21 09:57:22 +0530

    feat: deploy script now handles contract with constructor argument

--- scripts/main/deploy/handleDeployTx.js
+++ scripts/main/deploy/handleDeployTx.js
@@ -1,58 +1,54 @@
+const ethers = require("ethers")
 const {
-    fetchGasPriceLegacy,
-    fetchGasPriceEIP1559,
-  } = require("../utils/fetchData")
-const { ConstructorFragment } = require("@ethersproject/abi")
+  fetchGasPriceLegacy,
+  fetchGasPriceEIP1559,
+} = require("../utils/fetchData")
 
-  const handleDeployTx = async (
-    signer,
-    txType,
-    nonce,
-    metadata,
-    provider,
-    arrayOfArgs,
-    iface
-  ) => {
-    let gasLimit
+const handleDeployTx = async ({
+  signer,
+  txType,
+  nonce,
+  metadata,
+  arrayOfArgs,
+  provider,
+}) => {
+  const factory = new ethers.ContractFactory(
+    metadata.abi,
+    metadata.bytecode,
+    signer
+  )
+  const deployTransactionData = await factory.getDeployTransaction(
+    ...arrayOfArgs
+  ).data
+  try {
     if (txType === "1") {
       const gasData = await fetchGasPriceLegacy()
       maxFeeInGWEI = gasData.fastest
       maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
       // Get the estimated gas limit for this tx payload
-      gasLimit = await provider.estimateGas({
+      const gasLimit = await provider.estimateGas({
         type: 1,
         nonce: nonce,
-        data: iface.encodeFunctionData(ConstructorFragment, [
-          arrayOfArgs.toString(),
-      ]),
-        gasLimit: 14_999_999,
         gasPrice: maxFee,
+        gasLimit: 14_999_999,
+        data: deployTransactionData,
       })
       // Send transaction payload with the estimated gas limit
       const txPayload = {
         type: 1,
         nonce: nonce,
-        data: iface.encodeFunctionData(ConstructorFragment, [
-          arrayOfArgs.toString(),
-      ]),
-        gasLimit: gasLimit,
         gasPrice: maxFee,
+        gasLimit: gasLimit,
       }
-
-      const factory = new ContractFactory(metadata.abi, metadata.bytecode, signer)
-      const contract = await factory.deploy(txPayload)
+      const contract = await factory.deploy(...arrayOfArgs, txPayload)
       console.log(
         `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI`
-      ) 
+      )
       // Get transaction receipt
-      const tx = await contract.deployed()
-
-      console.log(`Deployment successful! Contract Address: ${contract.address}`)
-      console.log(`To verify your deployed smart contract: npx hardhat verify ${contract.address}`)
-
-      return tx.hash
+      await contract.deployed()
+      console.log(`Generated contract address: ${contract.address}\n`)
+      return (txHash = await contract.deployTransaction.hash)
     }
-  
     if (txType === "2") {
       const gasData = await fetchGasPriceEIP1559()
       maxFeeInGWEI = gasData.fast.maxFee
@@ -60,41 +56,35 @@ const { ConstructorFragment } = require("@ethersproject/abi")
       maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
       maxPriorityFee = Math.trunc(maxPriorityFeeInGWEI * 10 ** 9)
       // Get the estimated gas limit for this tx payload
-      gasLimit = await provider.estimateGas({
+      const gasLimit = await provider.estimateGas({
         type: 2,
-        data: iface.encodeFunctionData(ConstructorFragment, [
-          arrayOfArgs.toString(),
-      ]),
         nonce: nonce,
         gasLimit: 14_999_999,
-        maxPriorityFeePerGas: maxPriorityFee,
         maxFeePerGas: maxFee,
+        data: deployTransactionData,
+        maxPriorityFeePerGas: maxPriorityFee,
       })
       // Send transaction payload with the estimated gas limit
       const txPayload = {
         type: 2,
         nonce: nonce,
-        data: iface.encodeFunctionData(ConstructorFragment, [
-          arrayOfArgs.toString(),
-      ]),
         gasLimit: gasLimit,
-        maxPriorityFeePerGas: maxPriorityFee,
         maxFeePerGas: maxFee,
+        maxPriorityFeePerGas: maxPriorityFee,
       }
-      const factory = new ContractFactory(metadata.abi, metadata.bytecode, signer)
-      const contract = await factory.deploy(txPayload)
+      const contract = await factory.deploy(...arrayOfArgs, txPayload)
       console.log(
         `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI`
-      ) 
-      // Get transaction receipt 
-      tx = await contract.deployed()
-
-      console.log(`Deployment successful! Contract Address: ${contract.address}`)
-      console.log(`To verify your deployed smart contract: npx hardhat verify ${contract.address}`)
-
-      return tx.hash
+      )
+      // Get transaction receipt
+      await contract.deployed()
+      console.log(`Generated contract address: ${contract.address}\n`)
+      return (txHash = await contract.deployTransaction.hash)
     }
-    console.log(`Unsupported transaction type ${txType}`)
+  } catch (error) {
+    console.log(`Error in handleDeployTx: ${error}`)
+    process.exit(1)
   }
+}
 
 module.exports = handleDeployTx
