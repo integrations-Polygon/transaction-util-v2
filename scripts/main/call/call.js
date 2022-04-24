dbc04a486c5f1bad1d1fc8ed8de61380e9a86aca
grsLammy <grspro98@gmail.com>
2022-04-24 10:19:37 +0530

    perf(call script): reverted back to previous implementation

--- scripts/main/call/call.js
+++ scripts/main/call/call.js
@@ -1,89 +1,95 @@
-const walletAddress = process.env.PUBLIC_KEY
-const pKey = process.env.SIGNER_PRIVATE_KEY
-const projectID = process.env.PROJECT_ID
-const network = process.env.NETWORK
-const ps = require("prompt-sync")
-const prompt = ps()
-require("dotenv").config()
-const ethers = require("ethers")
-const handleCallTx = require("./handleCallTx")
-const isNumeric = require("../utils/isNumeric")
-const { fetchAbiData } = require("../utils/fetchData")
-const waitForConfirmation = require("../utils/waitForComfirmation")
+const walletAddress = process.env.PUBLIC_KEY;
+const pKey = process.env.SIGNER_PRIVATE_KEY;
+const projectID = process.env.PROJECT_ID;
+const network = process.env.NETWORK;
+const ps = require("prompt-sync");
+const prompt = ps();
+require("dotenv").config();
+const ethers = require("ethers");
+const handleCallTx = require("./handleCallTx");
+const isNumeric = require("../utils/isNumeric");
+const { fetchAbiData } = require("../utils/fetchData");
+const waitForConfirmation = require("../utils/waitForComfirmation");
 
 const contractFunctionCall = async ({
   txType,
   contractAddress,
+  functionName,
   arrayOfArgs,
 }) => {
   try {
-    let txHash
-    let retry = 0
-    let txReceipt = null
-    const provider = new ethers.providers.InfuraProvider(network, projectID)
-    const signer = new ethers.Wallet(pKey, provider)
-    const abiData = await fetchAbiData(contractAddress)
-    const abi = abiData.result
+    let txHash;
+    let retry = 0;
+    let txReceipt = null;
+    const provider = new ethers.providers.InfuraProvider(network, projectID);
+    const signer = new ethers.Wallet(pKey, provider);
+    const abiData = await fetchAbiData(contractAddress);
+    const abi = abiData.result;
+    const iface = new ethers.utils.Interface(abi);
     // Retry sending transaction utill success, 5 retries max
     while (txReceipt === null && retry < 5) {
-      const nonce = await provider.getTransactionCount(walletAddress)
+      const nonce = await provider.getTransactionCount(walletAddress);
       const userTxData = {
         signer,
         txType,
         contractAddress,
+        functionName,
         arrayOfArgs,
-        abi,
+        iface,
         nonce,
-      }
-      txHash = await handleCallTx(userTxData)
-      console.log("\nYour transaction is being mined...")
-      console.log(`The generated transaction hash is ${txHash}\n`)
-      console.log("You can check your transaction at:")
-      console.log(`https://polygonscan.com/tx/${txHash}\n`)
-      console.log("Waiting for 64 Block Confirmations\n")
+        provider,
+      };
+      txHash = await handleCallTx(userTxData);
+      console.log("\nYour transaction is being mined...");
+      console.log(`The generated transaction hash is ${txHash}\n`);
+      console.log("You can check your transaction at:");
+      console.log(`https://polygonscan.com/tx/${txHash}\n`);
+      console.log("Waiting for 64 Block Confirmations\n");
       // Wait for confirmation and get the txReceipt or null
-      txReceipt = await waitForConfirmation(provider, txHash)
+      txReceipt = await waitForConfirmation(provider, txHash);
       if (txReceipt === null) {
-        retry += 1
-        console.log("\nTransaction failed...Trying again!\n")
+        retry += 1;
+        console.log("\nTransaction failed...Trying again!\n");
       }
     }
     // Return the success txReceipt
-    if (txReceipt != null) return txReceipt
-    console.log("Transaction failed even after 5 retries")
+    if (txReceipt != null) return txReceipt;
+    console.log("Transaction failed even after 5 retries");
     // Return the failed txReceipt
-    return (txReceipt = await provider.getTransactionReceipt(txHash))
+    return (txReceipt = await provider.getTransactionReceipt(txHash));
   } catch (error) {
-    console.log("Error in contractFunctionCall", error)
-    process.exit(1)
+    console.log("Error in contractFunctionCall", error);
+    process.exit(1);
   }
-}
+};
 
 async function call() {
-  let arrayOfArgs = []
+  let arrayOfArgs = [];
   const txType = prompt(
     "Enter the transaction type (1 for legacy || 2 for EIP-1559): "
-  )
-  if (!txType) return console.log("Transaction type cannot be null")
+  );
+  if (!txType) return console.log("Transaction type cannot be null");
   if (txType !== "1" && txType !== "2")
-    return console.log(`Transaction type ${txType} is unsupported`)
+    return console.log(`Transaction type ${txType} is unsupported`);
   const contractAddress = prompt(
     "Enter the deployed & verified smart contract address: "
-  )
-  if (!contractAddress) return console.log("Contract address cannot be null")
+  );
+  if (!contractAddress) return console.log("Contract address cannot be null");
   if (contractAddress.length !== 42)
-    return console.log(`${contractAddress} is not a valid address`)
-  const totalArgs = prompt("Enter the total number of argument: ")
-  if (!totalArgs) return console.log("Total number of argument cannot be null")
-  if (isNumeric(totalArgs) === false) return console.log("Invalid input")
+    return console.log(`${contractAddress} is not a valid address`);
+  const functionName = prompt("Enter the name of the function to call: ");
+  if (!functionName) return console.log("Function name cannot be null");
+  const totalArgs = prompt("Enter the total number of argument: ");
+  if (!totalArgs) return console.log("Total number of argument cannot be null");
+  if (isNumeric(totalArgs) === false) return console.log("Invalid input");
   if (totalArgs !== 0) {
     for (i = 0; i < totalArgs; i++)
-      arrayOfArgs[i] = prompt(`Enter your argument [${i + 1}]: `)
+      arrayOfArgs[i] = prompt(`Enter your argument [${i + 1}]: `);
   }
-  console.log("\nFetching all the necessary data to start mining\n")
-  const userInputData = { txType, contractAddress, arrayOfArgs }
-  const txReceipt = await contractFunctionCall(userInputData)
-  return txReceipt
+  console.log("\nFetching all the necessary data to start mining\n");
+  const userInputData = { txType, contractAddress, functionName, arrayOfArgs };
+  const txReceipt = await contractFunctionCall(userInputData);
+  return txReceipt;
 }
 
-module.exports = call
+module.exports = call;
--- scripts/main/call/handleCallTx.js
+++ scripts/main/call/handleCallTx.js
@@ -1,75 +1,92 @@
-const { ethers } = require("ethers")
 const {
   fetchGasPriceLegacy,
   fetchGasPriceEIP1559,
-} = require("../utils/fetchData")
+} = require("../utils/fetchData");
 
 const handleCallTx = async ({
   signer,
   txType,
   contractAddress,
+  functionName,
   arrayOfArgs,
-  abi,
+  iface,
   nonce,
+  provider,
 }) => {
+  let gasLimit;
   try {
-    const contract = new ethers.Contract(contractAddress, abi, signer)
     if (txType === "1") {
-      const gasData = await fetchGasPriceLegacy()
-      maxFeeInGWEI = gasData.fastest
-      maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
+      const gasData = await fetchGasPriceLegacy();
+      maxFeeInGWEI = gasData.fastest;
+      maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9);
       // Get the estimated gas limit for this tx payload
-      const gasLimit = await contract.estimateGas.getName(...arrayOfArgs, {
+      gasLimit = await provider.estimateGas({
         type: 1,
+        to: contractAddress,
+        data: iface.encodeFunctionData(functionName.toString(), [
+          arrayOfArgs.toString(),
+        ]),
         nonce: nonce,
         gasLimit: 14_999_999,
         gasPrice: maxFee,
-      })
+      });
       // Send actual tx payload with estimated gas limit
       const txPayload = {
         type: 1,
+        to: contractAddress,
+        data: iface.encodeFunctionData(functionName.toString(), [
+          arrayOfArgs.toString(),
+        ]),
         nonce: nonce,
         gasLimit: gasLimit,
         gasPrice: maxFee,
-      }
-      const tx = await contract.getName(...arrayOfArgs, txPayload)
+      };
+      const tx = await signer.sendTransaction(txPayload);
       console.log(
         `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI`
-      )
-      return tx.hash
+      );
+      return tx.hash;
     }
     if (txType === "2") {
-      const gasData = await fetchGasPriceEIP1559()
-      maxFeeInGWEI = gasData.fast.maxFee
-      maxPriorityFeeInGWEI = gasData.fast.maxPriorityFee
-      maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9)
-      maxPriorityFee = Math.trunc(maxPriorityFeeInGWEI * 10 ** 9)
+      const gasData = await fetchGasPriceEIP1559();
+      maxFeeInGWEI = gasData.fast.maxFee;
+      maxPriorityFeeInGWEI = gasData.fast.maxPriorityFee;
+      maxFee = Math.trunc(maxFeeInGWEI * 10 ** 9);
+      maxPriorityFee = Math.trunc(maxPriorityFeeInGWEI * 10 ** 9);
       // Get the estimated gas limit for this tx payload
-      const gasLimit = await contract.estimateGas.getName(...arrayOfArgs, {
+      gasLimit = await provider.estimateGas({
         type: 2,
+        to: contractAddress,
+        data: iface.encodeFunctionData(functionName.toString(), [
+          arrayOfArgs.toString(),
+        ]),
         nonce: nonce,
         gasLimit: 14_999_999,
         maxPriorityFeePerGas: maxPriorityFee,
         maxFeePerGas: maxFee,
-      })
+      });
       // Send actual tx payload with estimated gas limit
       const txPayload = {
         type: 2,
+        to: contractAddress,
+        data: iface.encodeFunctionData(functionName.toString(), [
+          arrayOfArgs.toString(),
+        ]),
         nonce: nonce,
         gasLimit: gasLimit,
         maxPriorityFeePerGas: maxPriorityFee,
         maxFeePerGas: maxFee,
-      }
-      const tx = await contract.getName(...arrayOfArgs, txPayload)
+      };
+      const tx = await signer.sendTransaction(txPayload);
       console.log(
         `Your transaction is being mined and the gas price being used is ${maxFeeInGWEI} GWEI`
-      )
-      return tx.hash
+      );
+      return tx.hash;
     }
   } catch (error) {
-    console.log(`Error in handleCallTx: ${error}`)
-    process.exit(1)
+    console.log(`Error in handleCallTx: ${error}`);
+    process.exit(1);
   }
-}
+};
 
-module.exports = handleCallTx
+module.exports = handleCallTx;
